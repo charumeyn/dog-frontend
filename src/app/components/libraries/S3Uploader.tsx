1e6d9021cb8a38037769a60517bbc6f59506d052
charumeyn <charcandava@gmail.com>
2023-07-09 21:24:09 +0900

    feat: [Uploader] Create S3 uploader

---
+++ src/app/components/libraries/S3Uploader.tsx
@@ -0,0 +1,83 @@
+import { ChangeEventHandler, useCallback, useState } from "react";
+import { IconClose, IconPlus } from "../layout/Icons"
+import S3, { PutObjectRequest } from "aws-sdk/clients/s3";
+
+type S3UploaderProps = {
+  images: string[];
+  setImages: (images: string[]) => void;
+  mainImage: string;
+  setMainImage: (image: string) => void;
+  isMultiple?: boolean
+}
+
+const S3Uploader: React.FunctionComponent<S3UploaderProps> = ({ images, setImages, setMainImage, mainImage, isMultiple }) => {
+
+
+  const s3 = new S3({
+    accessKeyId: 'AKIAZQXK5AB4ADKPJKXG',
+    secretAccessKey: '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',
+    region: 'ap-northeast-2',
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleUpload: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    e.preventDefault();
+
+    let files = e.target.files!;
+
+    for (let i = 0; i < files.length; i++) {
+      try {
+        const upload = s3.upload({
+          Bucket: 'doggoslife',
+          Key: files.item(i)?.name,
+          Body: files.item(i),
+          ContentType: files.item(i)?.type,
+        } as PutObjectRequest);
+        setIsLoading(true);
+        await upload.promise().then((data) => images.push(data.Location));
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    setIsLoading(false);
+  };
+
+  const handleRemoveImage = useCallback((image: string) => {
+    const list = [...images]
+
+    if (list.indexOf(image) >= 0) {
+      const filtered = list.filter(x => x !== image)
+      setImages(filtered)
+      setMainImage("")
+    }
+  }, [setImages, images])
+
+  return (
+    <>
+      <div className="grid grid-cols-5">
+        {images?.map((image: string, i: number) =>
+          <div key={i} className={`${image == mainImage ? "border border-red-500" : ""} col-span-1 relative`}
+            onClick={() => setMainImage(image)}>
+            <span onClick={() => handleRemoveImage(image)}>
+              <IconClose className="w-5 h-5 absolute right-0 hover:cursor-pointer" />
+            </span>
+            <img src={image} alt={"image " + i} />
+          </div>
+        )}
+      </div>
+
+      <div>
+        {isLoading ? "loading" :
+          <div className="flex gap-x-4 text-teal-600 font-medium bg-teal-600 bg-opacity-10 border border-teal-600 border-opacity-200 px-5 py-3 rounded-lg">
+            <IconPlus className="w-5 h-5" />
+            <label htmlFor="files">Upload Images</label>
+            {/* <input id="files" className="hidden" type="file" onChange={handleUpload} multiple></input> */}
+          </div>
+        }
+      </div>
+    </>
+  )
+}
+
+export default S3Uploader;
--- src/app/feature/fundraisers/CreateContent.tsx
+++ src/app/feature/fundraisers/CreateContent.tsx
@@ -1,3 +1,4 @@
+import S3Uploader from "@/app/components/libraries/S3Uploader";
 import { RecipientType } from "@/app/types/enum/recipientType.enum";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -11,32 +12,15 @@ type CreateContentProps = {
   content: string;
   setContent: (content: string) => void;
 }
-const CreateContent: React.FunctionComponent<CreateContentProps> = ({ mainImage, setMainImage, images, setImages, content, setContent }) => {
+const CreateContent: React.FunctionComponent<CreateContentProps> = ({ images, setImages, mainImage, setMainImage, content, setContent }) => {
+
+
   return (
     <div className="px-6 py-6">
       <h3 className="text-gray-700 mb-2">Upload main image</h3>
-      <div className="grid grid-cols-3 gap-x-2">
-        <input
-          id="mainImage"
-          type="text"
-          name="mainImage"
-          value={mainImage}
-          onChange={(e: any) => setMainImage(e.target.value)}
-          className="border border-gray-300 px-3 py-2 rounded-lg"
-        />
-      </div>
 
       <h3 className="text-gray-700 mb-2">Upload more images</h3>
-      <div className="grid grid-cols-3 gap-x-2">
-        <input
-          id="images"
-          type="text"
-          name="images"
-          value={images}
-          onChange={(e: any) => setImages(e.target.value)}
-          className="border border-gray-300 px-3 py-2 rounded-lg"
-        />
-      </div>
+      <S3Uploader images={images} setImages={setImages} mainImage={mainImage} setMainImage={setMainImage} />
 
       <h3 className="text-gray-700 mb-2">Create content</h3>
       <div className="grid grid-cols-3 gap-x-2">
