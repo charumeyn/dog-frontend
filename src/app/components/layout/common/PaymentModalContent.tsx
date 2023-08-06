033a42e223b29399dc1e54c526250f524f87bed5
charumeyn <charcandava@gmail.com>
2023-08-06 11:35:55 +0900

    feat: [Donation] Payment modal & update colors

--- src/app/components/layout/common/PaymentModalContent.tsx
+++ src/app/components/layout/common/PaymentModalContent.tsx
@@ -1,15 +1,14 @@
-import { DonationCreateDto } from "@/app/types/dto/payment.dto";
 import { DonationType } from "@/app/types/enum/donationType.enum";
 import { PaymentGateway } from "@/app/types/enum/paymentGateway.enum";
-import { Fundraiser } from "@/app/types/fundraiser.interface";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import PaypalCheckout from "../../libraries/PaypalCheckout";
-import { useCreateDonation } from "@/app/hooks/api/useDonations";
-import { SuccessResult } from "@/app/types/apiResult";
 import StripeCheckout from "../../libraries/StripeCheckout";
-import { Donation } from "@/app/types/donation.interface";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import Input, { InputType } from "./Input";
+import Checkbox from "./Checkbox";
+import RadioButton from "./RadioButton";
+import Button from "./Button";
 
 type PaymentModalContentProps = {
   image: string;
@@ -21,97 +20,87 @@ type PaymentModalContentProps = {
 
 const PaymentModalContent: React.FunctionComponent<PaymentModalContentProps> = ({ image, name, type, recipient_id, setIsOpen }) => {
 
-  const [amount, setAmount] = useState<number | undefined>(0);
-  const [customAmount, setCustomAmount] = useState<number | undefined>(0);
-  const [selectedGateway, setSelectedGateway] = useState<PaymentGateway | undefined>();
   const amountOptions = [10, 20, 30, 50, 100]
-
-  const finalAmount = useMemo(() => {
-    if (amount != undefined || amount != 0) {
-      return amount
-    } else {
-      return customAmount
-    }
-  }, [amount, customAmount])
-
+  const [amount, setAmount] = useState<number | undefined>(0);
+  const [selectedGateway, setSelectedGateway] = useState<PaymentGateway | undefined>();
+  const [isChecked, setIsChecked] = useState<boolean>(false);
 
   const stripePromise = loadStripe(
     process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
   );
 
+  useEffect(() => {
+    if (isChecked) {
+      setAmount(0)
+    } else {
+      setAmount(10)
+    }
+  }, [isChecked, setAmount])
+
 
   return (
     <div>
-      <div className="bg-gray-100 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+      <div className="bg-neutral-100 px-4 pb-6 pt-8 sm:p-6 sm:pb-4">
         <img src={image} alt={name} className="w-20 h-20 rounded-full mx-auto" />
-        <h1 className="text-center">You are sponsoring <strong>{name}</strong>❤️</h1>
+        <h1 className="text-center mt-4 font-semibold text-lg text-neutral-900">You are sponsoring {name} ❤️</h1>
       </div>
 
-      <div className="px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-        <h2>Choose an amount</h2>
-        <div className="grid grid-cols-3 gap-2">
-          {amountOptions.map((option) =>
-            <div key={option}
-              onClick={() => {
-                setAmount(option)
-                setCustomAmount(0)
-              }}
-              className={`${option === amount ? "border-orange-600 bg-orange-600 text-white" : "hover:text-orange-600 hover:bg-orange-50 hover:border-orange-100"} grid-col-1 text-center border border-gray-200 rounded-md text-gray-500 font-medium hover:cursor-pointer py-3`}>
-              ${option}
-            </div>
-          )}
-          <input type="text"
-            name="customAmount"
-            id="customAmount"
-            value={customAmount}
-            onChange={(e: any) => {
-              setCustomAmount(Number(e.target.value))
-              setAmount(0)
-            }}
-            className="w-full border border-gray-200 rounded-md text-gray-800 font-medium py-3 px-3 focus:ring-0 focus:border-orange-500"
-            placeholder="Enter amount"
-          />
-        </div>
-      </div>
+      <div className="px-8 pb-8">
 
-      <div className="px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-        <h2>Payment Method</h2>
-        {Object.values(PaymentGateway).map((value) => (
-          <div
-            key={value}
-            className="flex items-center"
-          >
-            <input
-              id={value}
-              type="radio"
-              value={value}
-              name="status-radio"
-              className="w-4 h-4 "
-              onChange={() => setSelectedGateway(value)}
-              checked={selectedGateway === value}
-            />
-            <label
-              htmlFor={value}
-              className="text-sm font-medium text-gray-900 dark:text-gray-300 px-5 py-2 border-2 border-transparent peer-checked:border-2 peer-checked:rounded-3xl peer-checked:border-indigo-600 peer-checked:text-indigo-600 hover:cursor-pointer"
-            >
-              {value === PaymentGateway.Stripe ? "Credit or Debit" : value}
-            </label>
+        <div className="pt-6">
+          <h2 className="text-base font-semibold leading-7 text-neutral-900">Choose an amount</h2>
+          <div className="grid grid-cols-5 gap-3 mt-2">
+            {amountOptions.map((option) =>
+              <div key={option}
+                onClick={() => {
+                  setAmount(option)
+                }}
+                className={`${option === amount ? "bg-teal-600 text-white hover:bg-teal-500" : "ring-1 ring-inset ring-neutral-300  text-neutral-900 hover:bg-neutral-50"} 
+                ${isChecked ? "bg-neutral-200 pointer-events-none opacity-50" : ""}  flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase sm:flex-1 cursor-pointer`}>
+                {option}
+              </div>
+            )}
           </div>
-        ))}
-      </div>
-
-      {selectedGateway === PaymentGateway.Paypal ?
-        <div className="px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-          <PaypalCheckout type={DonationType.Dog} recipient_id={recipient_id} finalAmount={finalAmount} />
         </div>
-        :
-        selectedGateway === PaymentGateway.Stripe ?
-          <Elements stripe={stripePromise}>
-            <StripeCheckout type={DonationType.Dog} recipient_id={recipient_id} finalAmount={finalAmount} />
-          </Elements>
+
+        <div className="pt-4">
+          <Checkbox label="Enter custom amount" name="isChecked" isChecked={isChecked} setIsChecked={setIsChecked} />
+          {isChecked ?
+            <Input
+              type={InputType.Text}
+              name="customAmount"
+              placeholder="Enter amount"
+              onChange={(e: any) => {
+                setAmount(Number(e.target.value))
+              }}
+            />
+            : null}
+        </div>
+
+        <div className="pt-8">
+          <h2 className="text-base font-semibold leading-7 text-neutral-900">Payment Method</h2>
+          <div className="flex items-center space-x-10 space-y-0 mt-2">
+            {Object.values(PaymentGateway).map((value) =>
+              <RadioButton value={value} selected={selectedGateway} setSelected={setSelectedGateway} />
+            )}
+          </div>
+        </div>
+
+        {selectedGateway === PaymentGateway.Paypal ?
+          <PaypalCheckout type={DonationType.Dog} recipient_id={recipient_id} amount={amount} />
           :
-          null}
-    </div>
+          selectedGateway === PaymentGateway.Stripe ?
+            <Elements stripe={stripePromise}>
+              <StripeCheckout type={DonationType.Dog} recipient_id={recipient_id} amount={amount} />
+            </Elements>
+            :
+            <div className="col-span-2 mt-4">
+              <Button type="submit" text="Select a payment method" classNames="w-full" disabled={true} />
+            </div>
+        }
+
+      </div>
+    </div >
   )
 }
 
