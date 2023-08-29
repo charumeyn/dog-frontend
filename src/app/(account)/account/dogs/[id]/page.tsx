6dacbe163db1b22b7b41349a9233cb8d4d332a3b
charumeyn <charcandava@gmail.com>
2023-08-29 20:16:02 +0900

    fix: Update folder structure

--- src/app/(account)/account/dogs/[id]/page.tsx
+++ src/app/(account)/account/dogs/[id]/page.tsx
@@ -1,8 +1,6 @@
 
 import AccountContainer from "@/app/components/layout/AccountContainer";
-import DogsContent from "@/app/feature/account/DogsContent";
 import EditDogContent from "@/app/feature/account/EditDogContent";
-import RegisterDogContent from "@/app/feature/account/RegisterDogContent";
 
 export default function RegisterDog({ params }: { params: any }) {
 
