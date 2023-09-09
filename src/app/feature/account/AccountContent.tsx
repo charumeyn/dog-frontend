dddcf47979a4348ebfb461533ec8859a310318d0
charumeyn <charcandava@gmail.com>
2023-09-09 15:02:16 +0900

    fix: Update data types and success/error callbacks

--- src/app/feature/account/AccountContent.tsx
+++ src/app/feature/account/AccountContent.tsx
@@ -12,7 +12,7 @@ const AccountContent: React.FunctionComponent<AccountContentProps> = ({ account 
         <div className="w-full max-w-screen-2xl mx-auto px-4">
           <div className="max-w-4xl">
             <p className="mb-2 text-zinc-500 text-sm">Created by</p>
-            <div className="border border-zinc-100 rounded-lg px-5 py-5 mb-8">
+            <div className="border border-zinc-200 rounded-lg px-5 py-5 mb-8">
               <p className="text-sm">{account?.firstName}</p>
             </div>
 
