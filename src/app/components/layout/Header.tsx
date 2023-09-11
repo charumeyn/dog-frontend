ebca5f29f7faf409a8da8cf81acb3f4ef2204037
charumeyn <charcandava@gmail.com>
2023-09-11 23:47:00 +0900

    feat: [Responsive] Header UI

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,65 +1,109 @@
 "use client"
 
-import { useAccount, useLogout } from "@/app/hooks/api/useAuth";
-import { useRouter } from "next/navigation";
-import { useCallback, useMemo } from "react";
-import Container, { ContainerType } from "./Container";
-import { User, UserType } from "@/app/types/user.interface";
-import DropdownMenu from "./common/DropdownMenu";
-import { IconChevronDown, IconHorizontalDots } from "./Icons";
+import { Fragment, useCallback, useMemo } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { IconChevronDown, IconClose, IconMenu, IconUser } from './Icons'
+import { User, UserType } from '@/app/types/user.interface'
+import { useParams, usePathname, useRouter } from 'next/navigation'
+import { useAccount, useLogout } from '@/app/hooks/api/useAuth'
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Header() {
 
+  const pathName = usePathname();
+
+  const path = useMemo(() => {
+    if (pathName.includes('dogs')) {
+      return 'dogs'
+    } else if (pathName.includes('fundraisers')) {
+      return 'fundraisers'
+    } else if (pathName.includes('posts')) {
+      return 'posts'
+    } else return
+  }, [pathName])
+
   const { data: account, isLoading } = useAccount();
 
-  const menu = [
+  const mainMenu = [
     {
-      name: 'Dogs',
-      url: '/dogs'
+      path: "dogs",
+      url: "/dogs",
+      label: "Dogs"
     },
     {
-      name: 'Fundraisers',
-      url: '/fundraisers'
+      path: "fundraisers",
+      url: "/fundraisers",
+      label: "Fundraisers"
     },
     {
-      name: 'Doggo Diaries',
-      url: '/posts'
+      path: "posts",
+      url: "/posts",
+      label: "Doggo Diaries"
     }
   ]
 
-  const isLoggedIn = useMemo(() => {
-    return account && account.id;
-  }, [account])
-
-
   return (
-    <div className="shadow-xl">
-      <Container
-        type={ContainerType.SingleColumn}
-        className="sticky top-0"
-        mainContent={
-          <div className="flex justify-between">
-            <a href="/" className="text-teal-600 hover:text-teal-700 py-5 text-2xl font-extrabold">Doggo&apos;s Life</a>
-            <div className="flex items-center">
-              {menu.map((item: any, i: number) =>
-                <a className="px-8 py-5 hover:text-teal-600 font-medium" key={i} href={item.url}>{item.name}</a>
-              )}
-            </div>
-            <div className="flex">
-              {isLoading ? null :
-                isLoggedIn && account ?
-                  <Username account={account} />
-                  :
-                  null
-              }
-              {!isLoggedIn ? <a className="border-l border-zinc-200 py-5 px-8" href="/login">Login</a> : null}
+    <Disclosure as="nav" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <IconClose />
+                  ) : (
+                    <IconMenu />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <a href="/" className="text-teal-600 hover:text-teal-700 py-5 text-2xl font-extrabold">Doggo&apos;s Life</a>
+                </div>
+                <div className="hidden sm:flex sm:space-x-6 justify-center flex-1">
+                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  {mainMenu.map((menu) => (
+                    <a
+                      href={menu.url}
+                      className={`${path === menu.path ? "border-teal-500 text-gray-900" : "text-gray-500 border-transparent hover:border-gray-300 hover:text-gray-700"} inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium`}
+                    >
+                      {menu.label}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              {account ?
+                <Username account={account} /> : null}
             </div>
           </div>
-        } />
-    </div >
 
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 pb-4 pt-2">
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+              {mainMenu.map((menu) => (
+                <Disclosure.Button
+                  as="a"
+                  href={menu.url}
+                  className={`${path === menu.path ? "bg-teal-50 border-teal-500 text-teal-700" : "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700"} block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium`}
+                >
+                  {menu.label}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
+
 
 
 function Username({ account }: { account: User }) {
@@ -69,6 +113,10 @@ function Username({ account }: { account: User }) {
   const onLogoutError = () => console.log("error logging out")
 
   const { mutate: logout } = useLogout(onLogoutSuccess, onLogoutError);
+
+  const isLoggedIn = useMemo(() => {
+    return account && account.id;
+  }, [account])
 
   const shelterMenu = [
     { label: "Dogs", url: "/account/dogs" },
@@ -87,50 +135,89 @@ function Username({ account }: { account: User }) {
     { label: "Logout", onClick: () => logout() },
   ];
 
+
   return (
-    <>
-      <div className="flex items-center text-sm pr-5">
-        {account.type === UserType.Shelter ?
-          <a className="text-teal-600 hover:border-b hover:border-teal-600" href="/posts/create">+ Create Post</a> :
-          <a className="text-teal-600 hover:border-b hover:border-teal-600" href="/fundraisers/create">+ Create Fundraiser</a>
-        }
-      </div>
-      <div className="flex border-l border-zinc-200 ">
-        {account.type === UserType.Shelter ?
-          <div className="flex items-center gap-3 pl-5">
-            <img src={account?.shelter.mainImage} className="inline-block h-10 w-10 rounded-full ring-2 ring-white" />
-            <p className="flex flex-col">
-              <span className="text-orange-700 text-xs font-medium">Shelter</span>
-              <a href="/account/dogs">{account?.shelter.name}</a>
-            </p>
-            <DropdownMenu
-              menuItems={shelterMenu}
-              icon={
-                <IconChevronDown
-                  className="h-8 w-8 p-1.5 rounded-full text-zinc-600"
-                  aria-hidden="true"
-                />
-              }
-            />
+    isLoggedIn ?
+      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <button
+          type="button"
+          className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+        >
+          <span className="absolute -inset-1.5" />
+          <span className="sr-only">View notifications</span>
+        </button>
+
+        {/* Profile dropdown */}
+        <Menu as="div" className="relative ml-3">
+          <div>
+            <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 items-center gap-2 text-zinc-600">
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="h-8 w-8 rounded-full"
+                src={account.type === UserType.User ? account.image : account.shelter.mainImage}
+                alt=""
+              />
+              <span className="hidden md:flex items-center gap-x-1 text-left">
+                {account.type === UserType.User ?
+                  <a href="/account">{account?.firstName}</a>
+                  :
+                  <p className="flex flex-col">
+                    <span className="text-orange-700 text-xs font-medium">Shelter</span>
+                    <a href="/account/dogs">{account?.shelter.name}</a>
+                  </p>
+                }
+                <IconChevronDown className="w-4 h-4 text-zinc-500" />
+              </span>
+            </Menu.Button>
           </div>
-          :
-          <div className="flex items-center gap-3 pl-5">
-            <p className="flex items-center gap-3">
-              <img src={account?.image} className="inline-block h-10 w-10 rounded-full ring-2 ring-white" />
-              <a href="/account">Hi, {account?.firstName}!</a>
-            </p>
-            <DropdownMenu
-              menuItems={userMenu}
-              icon={
-                <IconChevronDown
-                  className="h-8 w-8 p-1.5 rounded-full text-zinc-600"
-                  aria-hidden="true"
-                />
-              }
-            />
-          </div>
-        }
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              {account.type === UserType.User ?
+                userMenu.map((menuItem, i) => (
+                  <Menu.Item key={i}>
+                    {({ active }) => (
+                      <a
+                        onClick={menuItem.onClick}
+                        href={menuItem.url}
+                        className={`${active ? "bg-gray-100 text-gray-900" : "text-gray-700"} block px-4 py-2 text-sm hover:cursor-pointer`}
+                      >
+                        {menuItem.label}
+                      </a>
+                    )}
+                  </Menu.Item>
+                )) :
+                shelterMenu.map((menuItem, i) => (
+                  <Menu.Item key={i}>
+                    {({ active }) => (
+                      <a
+                        onClick={menuItem.onClick}
+                        href={menuItem.url}
+                        className={`${active ? "bg-gray-100 text-gray-900" : "text-gray-700"} block px-4 py-2 text-sm hover:cursor-pointer`}
+                      >
+                        {menuItem.label}
+                      </a>
+                    )}
+                  </Menu.Item>
+                ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
-    </>
+      :
+      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <a href="/login"
+          className="hover:cursor-pointer inline-flex gap-2 items-center px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">
+          <IconUser /><span className="hidden md:inline">Log in</span>
+        </a>
+      </div>
   )
 }
--- src/app/components/layout/Icons.tsx
+++ src/app/components/layout/Icons.tsx
@@ -5,8 +5,11 @@ type IconProps = {
 
 export function IconClose(props: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={props.fill ? props.fill : "currentColor"} className={props.className ? props.className : "w-5 h-5"}>
-      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+    // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill={props.fill ? props.fill : "currentColor"} className={props.className ? props.className : "w-5 h-5"}>
+    //   <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+    // </svg>
+    <svg className={props.className ? props.className : "w-5 h-5"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
   );
 }
@@ -240,3 +243,18 @@ export function IconComment(props: IconProps) {
   )
 }
 
+export function IconMenu(props: IconProps) {
+  return (
+    <svg className={props.className ? props.className : "w-5 h-5"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
+  )
+}
+
+export function IconUser(props: IconProps) {
+  return (
+    <svg className={props.className ? props.className : "w-5 h-5"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  )
+}
